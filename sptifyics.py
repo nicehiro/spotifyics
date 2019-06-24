6e9580067b4ae0ab6ab49d56cc282512f14cb403
方圆 <wfy11235813@gmail.com>
2019-06-24 17:01:46 +0800

    parse lyric

--- sptifyics.py
+++ sptifyics.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
-from spotifydbus import SpotifyDBus
 from netrequest import NeteaseRequest
 
 
 if __name__ == '__main__':
-    spfy = SpotifyDBus()
     netease_req = NeteaseRequest()
     original_lyric, transed_lyric = netease_req.get_lyrics()
     print(original_lyric, transed_lyric)
