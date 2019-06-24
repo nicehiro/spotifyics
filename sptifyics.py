4b8e78948ad94e790dba2f19368a7b60cca1defc
hiro <wfy11235813@gmail.com>
2019-06-24 22:32:56 +0800

    Add README.

--- sptifyics.py
+++ sptifyics.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python
 from netrequest import NeteaseRequest
+from lyric_parser import LyricParser
 
 
 if __name__ == '__main__':
     netease_req = NeteaseRequest()
     original_lyric, transed_lyric = netease_req.get_lyrics()
-    print(original_lyric, transed_lyric)
+    parser = LyricParser(original_lyric, transed_lyric)
+    lyric_dict = parser.parse()
+    for value in lyric_dict.values():
+        print(value)
