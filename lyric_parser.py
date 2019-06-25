d05f5b12044d02231a8248162e5d385fa6b43e29
hiro <wfy11235813@gmail.com>
2019-06-25 21:21:02 +0800

    Make a simple GUI.

--- lyric_parser.py
+++ lyric_parser.py
@@ -5,13 +5,17 @@ from collections import OrderedDict
 class LyricParser(object):
     """Parse Netease Mysic API lyric."""
 
-    def __init__(self, lyric_ori, lyric_transed, has_trans=True):
+    def __init__(self, has_trans=True):
         self.pattern = re.compile('\[(\d+):(\d+).(\d+)\](.*)\n')
-        self.lyric_origin = lyric_ori
-        self.lyric_translate = lyric_transed
         self.has_trans = has_trans
 
     def parse2dict(self, lyric):
+        """Parse lyrics json file to Dict.
+
+        :return dict
+            key: millseconds of lyric position
+            value: lyric
+        """
         lyric_list = self.pattern.findall(lyric)
         d = OrderedDict()
         for line in lyric_list:
@@ -20,12 +24,23 @@ class LyricParser(object):
             d[mills] = txt
         return d
 
-    def parse(self):
-        lyric_ori_dict = self.parse2dict(self.lyric_origin)
+    def parse(self, lyric_origin, lyric_translate):
+        """
+        Concat original lyric with translated lyric.
+
+        :param lyric_origin: original lyric
+        :param lyric_translate: translated lyric
+        :return: dict
+            key: millseconds of lyric position
+            value: lyric
+        """
         lyric_transed_dict = OrderedDict()
-        if self.has_trans and self.lyric_translate:
-            lyric_transed_dict = self.parse2dict(self.lyric_translate)
-        for key in lyric_ori_dict:
-            lyric_ori_dict[key] += '\n' + lyric_transed_dict[key] + '\n' \
-                if key in lyric_transed_dict else '\n'
+        if self.has_trans and lyric_translate:
+            lyric_transed_dict = self.parse2dict(lyric_translate)
+        lyric_ori_dict = None
+        if lyric_origin:
+            lyric_ori_dict = self.parse2dict(lyric_origin)
+            for key in lyric_ori_dict:
+                lyric_ori_dict[key] += '\n' + lyric_transed_dict[key] + '\n' \
+                    if key in lyric_transed_dict else '\n'
         return lyric_ori_dict
